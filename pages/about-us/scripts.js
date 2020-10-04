a3329866d777a0cbb3ca73ebf907aea093de382e
Buzzgrow <artur_pg2011@hotmail.com>
2020-10-04 12:06:07 -0300

    enhancing about-us page

--- pages/about-us/scripts.js
+++ pages/about-us/scripts.js
@@ -2,6 +2,7 @@
   'use strict';
 
   const $githubSection = doc.querySelector('#github');
+  const $main = doc.querySelector('main');
 
   const urls = [
     'https://api.github.com/users/freirart',
@@ -13,9 +14,16 @@
   urls.forEach(async function(url){
     const profile = await (await fetch(url)).json();
     const article = `
-      <article>
-        <h3>${profile.name}</h3>
+      <article class="profile">
         <img src="${profile.avatar_url}">
+        <div class="content">
+          <h3>${profile.name}</h3>
+          <p>${profile.bio}</p>
+          <div class="btn-group">
+            <a href="${profile.blog}"><i class="fab fa-linkedin"></i></a>
+            <a href="${profile.html_url}"><i class="fab fa-github"></i></a>
+          </div>
+        </div>
       </article>
     `
     $githubSection.innerHTML += article;
