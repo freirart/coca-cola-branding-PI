4e2f614418a429103e1d9942643e8deb0c833ca5
Buzzgrow <artur_pg2011@hotmail.com>
2020-10-09 20:55:31 -0300

    adding open/close nav func

--- pages/about-us/scripts.js
+++ pages/about-us/scripts.js
@@ -2,12 +2,15 @@
   'use strict';
 
   const $githubSection = doc.querySelector('#github');
+  const $nav = doc.querySelector('nav');
+  const $main = doc.querySelector('main');
+  const $navButton = doc.querySelector('nav button');
 
   const urls = [
     'https://api.github.com/users/freirart',
-    'https://api.github.com/users/MartinObrecht',
     'https://api.github.com/users/beatricoli',
     'https://api.github.com/users/PamAvelar',
+    'https://api.github.com/users/eduardo-assimo'
   ];
 
   urls.forEach(async function(url){
@@ -27,5 +30,19 @@
     `
     $githubSection.innerHTML += article;
   });
+
+  $navButton.addEventListener('click', openNav, false);
+
+  function openNav() {
+    $nav.classList.toggle('opened');
+    $main.classList.toggle('opened');
+    $navButton.classList.toggle('opened');
+    $navButton.firstElementChild.classList.toggle('fa-bars');
+    $navButton.firstElementChild.classList.toggle('fa-times');
+    
+    doc.querySelectorAll('nav p').forEach(link => {
+      link.classList.toggle('hidden');
+    });
+  }
 
 })(window, document)
